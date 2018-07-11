03f16f609909cd8ab17effb4099c817cfe9cecef
mbertling <masha.bertling@gmail.com>
2018-07-11 12:11:40 -0400

    Cleaned the code for FIRC with correct pval

--- R/FIRC_method_n.R
+++ R/FIRC_method_n.R
@@ -25,11 +25,47 @@ analysis.FIRC <- function( Yobs, Z, sid, data=df, REML = FALSE ) {
                            method = method,
                            control=nlme::lmeControl(opt="optim", returnObject=TRUE))
   
-  td = abs(as.numeric( deviance( re.mod ) - deviance( re.mod.null )))
+  td = abs(as.numeric( deviance( re.mod.null ) - deviance( re.mod )))
   0.5 * pchisq(td, 1, lower.tail = FALSE )
 }
 
+# FIRC function with correct pval
+analysis.FIRC <- function( df ) {
+  
+  #fit multilevel model and extract pvalue
+  re.mod <- nlme::lme(Yobs ~ 0 + Z + sid,
+                      data = df,
+                      random = ~ 0 + Z | sid,
+                      weights = nlme::varIdent(form = ~ 1 | Z), na.action=na.exclude,
+                      method="ML",
+                      control=nlme::lmeControl(opt="optim",returnObject=TRUE))
+  
+  # Test for cross site variation
+  re.mod.null <- nlme::gls(Yobs ~ 0 + Z + sid,
+                           data=df,
+                           weights = nlme::varIdent(form = ~ 1 | Z), na.action=na.exclude,
+                           method = "ML",
+                           control=nlme::lmeControl(opt="optim", returnObject=TRUE))
+  
+  
 
+  # prtAnova = anova(re.mod, re.mod.null)
+  # prtAnova$'p-value'
+  
+  # myanova = anova(re.mod, re.mod.null)
+  # myanova[["p-value"]]
+ 
+  myanova = anova(re.mod.null, re.mod)
+  myanova[2,9]
+  
+  # deviance.rand = (-2)*logLik(re.mod) # keeping here so we have it if needed
+  # deviance.null = (-2)*logLik(re.mod.null)
+  # td = abs(as.numeric( deviance.rand - deviance.null))
+  # 0.5 * pchisq(td, 1, lower.tail = FALSE )
+  # 
+  # pval = summary(myanova)[2,9]
+  # pval
+}
 
 # Testing
 if ( FALSE ) {
@@ -39,8 +75,8 @@ if ( FALSE ) {
   # head( df )
   # describe.data( df )
 
-  analysis.FIRC( Yobs, Z, sid, df )
+  analysis.FIRC( df )
+  anova(re.mod.null, re.mod)
   
 }
-
 
