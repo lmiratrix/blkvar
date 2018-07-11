4f64f9979c6cd672f5a060759f4b0b2245e88666
mbertling <masha.bertling@gmail.com>
2018-07-11 12:06:24 -0400

    Included the code with no cov

--- R/MoM_method.R
+++ R/MoM_method.R
@@ -5,12 +5,11 @@
 #'
 
 #' We have:
-#'   * Unweighted MoM
-#'   * Weighted MoM
+#'   * Unweighted MoM for both simulations (with and without covariate)
+#'   * Weighted MoM for both simulations (with and without covariate)
 #'
 
 
-## MoM Unweighted 
 ## MoM Unweighted 
 analysis.MoM.unweighted = function( df ){
   s = max(as.numeric(as.character(df$sid)))
@@ -45,6 +44,7 @@ analysis.MoM.unweighted = function( df ){
 }
 
 
+
 ## MoM weighted 
 analysis.MoM = function( df ){
   s = max(as.numeric(as.character(df$sid)))
@@ -68,7 +68,7 @@ analysis.MoM = function( df ){
   bj = coef_table[hetero_vars,"Estimate"]
   vj = coef_table[hetero_vars,"Std. Error"]^2
   
-  browser()
+  #browser()
   
   ## estimate q
   wj = 1/vj
@@ -91,6 +91,79 @@ analysis.MoM = function( df ){
   
   ## calculate tau^2 hat
   tausq = (Q - (s - 1))/c
+
   
   return(tausq)
+}
+
+
+
+## MoM Unweighted, no covarite 
+analysis.MoM.unweighted = function( df ){
+  s = length( unique( df$sid ) ) # max(as.numeric(as.character(df$sid)))
+  df$sid = as.factor(df$sid)
+  
+  # get the model matrix we need to estimate the relevant betas
+  Mtest = lm( Yobs ~ sid*Z - 1 - Z, data=df)
+  
+  # Tidy this up!
+  coef_table = summary(Mtest)$coef
+  hetero_vars = grep("Z",rownames(coef_table))
+  
+  ### pool treatment effect estimates and compute their variance estimates
+  bj = coef_table[hetero_vars,"Estimate"]
+  vj = coef_table[hetero_vars,"Std. Error"]^2
+  
+  ## Var bj
+  var_bj = var(bj)
+  # Average variance bj
+  mean_vj = mean(vj)
+  
+  MoM_unweighted = var_bj - mean_vj
+  
+  return(MoM_unweighted)
+}
+
+## MoM weighted, no covariate 
+analysis.MoM.weighted = function( df ){
+  s = length( unique( df$sid ) ) # max(as.numeric(as.character(df$sid)))
+  df$sid = as.factor(df$sid)
+  
+  # get the model matrix we need to estimate the relevant betas
+  Mtest = lm( Yobs ~ sid*Z - 1 - Z, data=df)
+  
+  # Tidy this up!
+  coef_table = summary(Mtest)$coef
+  hetero_vars = grep("Z",rownames(coef_table))
+  
+  ### pool treatment effect estimates and compute their variance estimates
+  bj = coef_table[hetero_vars,"Estimate"]
+  vj = coef_table[hetero_vars,"Std. Error"]^2
+  
+  #browser() = command to test whether everything within the function works
+  
+  ## estimate q and pvalue
+  wj = 1/vj
+  wjsq = wj^2
+  
+  ## weight*site-specific treatment effect
+  wjT = wj*bj
+  wjTsq = wj*(bj^2)
+  
+  ## calculate Q
+  sumwj = sum(wj) #total weights
+  sumwjsq = sum(wjsq) # total weight squared
+  sumwjT = sum(wjT) #sum of weights interecations with site-specific effect
+  sumwjTsq = sum(wjTsq) #same but for squared
+  sumwjT_sq = sumwjT^2 #and we square again
+  Q = sumwjTsq - sumwjT_sq/sumwj
+  
+  ## calculate c
+  c = sumwj - (sumwjsq)/sumwj
+  
+  ## calculate tau^2 hat
+  tausq = (Q - (s - 1))/c
+  
+  return(tausq)
+  
 }
