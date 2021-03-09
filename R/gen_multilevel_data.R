8b8f9dc42c153126a7b16ccf5e5c810bc7e62e90
Luke Miratrix <lmiratrix@gmail.comm>
2021-03-09 14:42:52 -0500

    Split data generator to allow for individual generation as a second step.

--- R/gen_multilevel_data.R
+++ R/gen_multilevel_data.R
@@ -55,6 +55,94 @@ rerandomize_data <- function(dat) {
 
 
 
+#' Generate individual data given a dataframe of site level characteristics.
+#'
+#' @param sdat Dataframe of site level characteristics
+#'
+#' @describeIn generate_multilevel_data_model Part of data generation that
+#'   generates individual level covariates.
+#'
+#' @return Dataframe of indiivdual level data
+#' @export
+#'
+generate_individual_data = function( sdat, p = 0.5,
+                                     sigma2.e = 1,
+                                     beta.X = NULL,
+                                     variable.p = FALSE,
+                                     cluster.rand = FALSE,
+                                     sigma2.mean.X = 0,
+                                     proptx.impact.correlate = FALSE,
+                                     verbose = FALSE) {
+
+    include_X <- FALSE
+    if (!is.null(beta.X)) {
+        include_X <- TRUE
+    }
+
+    J = nrow( sdat )
+    nj = sdat$n
+    beta.0j = sdat$beta.0
+    beta.1j = sdat$beta.1
+    Wj = sdat$W
+
+    Zj = NULL
+    if ( cluster.rand ) {
+        stopifnot( !is.null( sdat$Z ) )
+        Zj = sdat$Z
+    }
+
+    # generate individual level outcomes
+    sid <- as.factor(rep(1:J, nj))
+    N <- sum(nj)
+    # randomize units within each site (proportion p to treatment)
+    dd <- data.frame(sid = sid)
+    if (variable.p) {
+        ths <- min(p, 1 - p) * 0.75
+        ps <- runif(J, p - ths, p + ths)
+
+        if (proptx.impact.correlate != 0) {
+            ps <- sort( ps, decreasing = (proptx.impact.correlate < 0))
+        }
+
+        # threshold to ensure we always have 2 units in tx and co for all blocks
+        # regardless of assigned p
+        ps <- pmin((nj - 2) / nj, pmax(2 / nj, ps))
+        dd$p <- ps[as.numeric(dd$sid)]
+    } else if (proptx.impact.correlate != 0) {
+        warning( "Can't have proportion treated correlated with impact if the proportion treated does not vary." )
+    }
+    if (cluster.rand) {
+        dd$Z <- Zj[ dd$sid ]
+    } else {
+        dd <- dd %>% group_by( sid ) %>% mutate(Z = as.numeric(sample(n()) <= n() * p))
+    }
+    Zij <- dd$Z
+    dd$p <- NULL
+
+    # individual covariates and residuals
+    if (include_X) {
+        stopifnot(sigma2.mean.X < 1)
+        Xbar <- rnorm(J, mean = 0, sd = sqrt(sigma2.mean.X))
+        X <- rnorm(N, mean = Xbar[sid], sd = sqrt(1 - sigma2.mean.X))
+        stopifnot(sigma2.e - beta.X ^ 2 > 0)
+        e <- rnorm(N, mean = 0, sd = sqrt(sigma2.e - beta.X ^ 2))
+        Y0 <- beta.0j[sid] + beta.X * X + e
+    } else {
+        e <- rnorm(N, mean = 0, sd = sqrt(sigma2.e))
+        Y0 <- beta.0j[sid] + e
+    }
+    Y1 <- Y0 + beta.1j[sid]
+    df <- data.frame(sid = as.factor(sid), Y0 = Y0, Y1 = Y1, Z = Zij, Yobs = ifelse(Zij, Y1, Y0))
+    if (include_X) {
+        df$X <- X
+    }
+    if ( !is.null( Wj ) ) {
+        df$W <- Wj[df$sid]
+    }
+    df
+}
+
+
 
 #' @title Generate multilevel data from a model
 #'
@@ -135,11 +223,7 @@ generate_multilevel_data_model <- function(n.bar = 10, J = 30, p = 0.5,
         include_W <- FALSE
     }
 
-    if (!is.null(beta.X)) {
-        include_X <- TRUE
-    } else {
-        include_X <- FALSE
-    }
+
 
     # Generate average control outcome and average ATE for all sites
     Sigma <- matrix(c(tau.00, tau.01, tau.01, tau.11), nrow = 2)
@@ -183,67 +267,28 @@ generate_multilevel_data_model <- function(n.bar = 10, J = 30, p = 0.5,
         Zj <- 0 + (sample(J) <= J * p)
     }
 
+    if (cluster.rand) {
+        df <- data.frame(n = nj, W = Wj, beta.0 = beta.0j, beta.1 = beta.1j, u0 = mv[,1], u1 = mv[,2], Zj = Zj)
+    } else {
+        df <- data.frame(n = nj, W = Wj, beta.0 = beta.0j, beta.1 = beta.1j, u0 = mv[,1], u1 = mv[,2])
+    }
+
     if (return.sites) {
-        if (cluster.rand) {
-            df <- data.frame(n = nj, W = Wj, beta.0 = beta.0j, beta.1 = beta.1j, u0 = mv[,1], u1 = mv[,2], Zj = Zj)
-        } else {
-            df <- data.frame(n = nj, W = Wj, beta.0 = beta.0j, beta.1 = beta.1j, u0 = mv[,1], u1 = mv[,2])
-        }
         if (!include_W) {
             df$W <- NULL
         }
+
         return( df )
     } else {
-        # generate individual level outcomes
-        sid <- as.factor(rep(1:J, nj))
-        N <- sum(nj)
-        # randomize units within each site (proportion p to treatment)
-        dd <- data.frame(sid = sid)
-        if (variable.p) {
-            ths <- min(p, 1 - p) * 0.75
-            ps <- runif(J, p - ths, p + ths)
+        df <- generate_individual_data( df, p = p, sigma2.e = sigma2.e, beta.X = beta.X,
+                                        proptx.impact.correlate = proptx.impact.correlate,
+                                        sigma2.mean.X = sigma2.mean.X,
+                                        variable.p = variable.p, cluster.rand = cluster.rand,
+                                        verbose = verose )
 
-            if (proptx.impact.correlate != 0) {
-                ps <- sort( ps, decreasing = (proptx.impact.correlate < 0))
-            }
-
-            # threshold to ensure we always have 2 units in tx and co for all blocks
-            # regardless of assigned p
-            ps <- pmin((nj - 2) / nj, pmax(2 / nj, ps))
-            dd$p <- ps[as.numeric(dd$sid)]
-        } else if (proptx.impact.correlate != 0) {
-            warning( "Can't have proportion treated correlated with impact if the proportion treated does not vary." )
-        }
-        if (cluster.rand) {
-            dd$Z <- Zj[ dd$sid ]
-        } else {
-            dd <- dd %>% group_by( sid ) %>% mutate(Z = as.numeric(sample(n()) <= n() * p))
-        }
-        Zij <- dd$Z
-        dd$p <- NULL
-
-        # individual covariates and residuals
-        if (include_X) {
-            stopifnot(sigma2.mean.X < 1)
-            Xbar <- rnorm(J, mean = 0, sd = sqrt(sigma2.mean.X))
-            X <- rnorm(N, mean = Xbar[sid], sd = sqrt(1 - sigma2.mean.X))
-            stopifnot(sigma2.e - beta.X ^ 2 > 0)
-            e <- rnorm(N, mean = 0, sd = sqrt(sigma2.e - beta.X ^ 2))
-            Y0 <- beta.0j[sid] + beta.X * X + e
-        } else {
-            e <- rnorm(N, mean = 0, sd = sqrt(sigma2.e))
-            Y0 <- beta.0j[sid] + e
-        }
-        Y1 <- Y0 + beta.1j[sid]
-        df <- data.frame(sid = as.factor(sid), Y0 = Y0, Y1 = Y1, Z = Zij, Yobs = ifelse(Zij, Y1, Y0))
-        if (include_X) {
-            df$X <- X
-        }
-        if (include_W) {
-            df$W <- Wj[df$sid]
-        }
         attr(df, "tau.S") <- sd( beta.1j)
-        df
+
+        return( df)
     }
 }
 
@@ -290,7 +335,10 @@ generate_multilevel_data <- function(n.bar = 10, J = 30, p = 0.5,
         scat( "tau.11* <- %.2f\n", gamma.11 ^ 2 * sigma2.W + tau.11)
         scat( "sigma2.e* <- %.2f\n", sigma2.e)
     }
-    generate_multilevel_data_model(n.bar = n.bar, J = J, p = p, gamma.00, gamma.01, gamma.10, gamma.11, tau.00, tau.01, tau.11, sigma2.e, verbose = verbose, ...)
+    generate_multilevel_data_model(n.bar = n.bar, J = J, p = p,
+                                   gamma.00 = gamma.00, gamma.01 = gamma.01, gamma.10 = gamma.10, gamma.11 = gamma.11,
+                                   tau.00 = tau.00, tau.01 = tau.01, tau.11 = tau.11, sigma2.e = sigma2.e,
+                                   verbose = verbose, ...)
 }
 
 
