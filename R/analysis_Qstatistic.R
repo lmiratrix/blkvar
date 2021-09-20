a70bc1d30754af7ed0801d6d6fb595b23e807b73
Luke Miratrix <lmiratrix@gmail.comm>
2021-09-20 12:21:45 -0400

    Cleaned up q-stat code and added a class to the result object from the qstat call.

--- R/analysis_Qstatistic.R
+++ R/analysis_Qstatistic.R
@@ -22,16 +22,27 @@ find_p_value = function( tau, resid2, vj ) {
 }
 
 
+# Q(tau) is a monotonic decreasing function.
+#
+# This method conducts a search to find value of tau that makes Q(tau) equal to
+# the passed threshold.
+#
+# tau_start and tau are fenceposts for this binary search. If bracketed=FALSE,
+# then keep doubling tau until the Q-stat is lower than the threshold.
 find_CI_endpoint = function( resid2, vj, threshold, tau_start, tau,
                              tolerance = 0.01,
                              bracketed = FALSE ) {
 
-    qmax = sum( resid2 / vj )
+    stopifnot( tau_start < tau )
+
+    # Q-stat with no hypothesized cross site variation will be largest Q
+    # possible
+    qmax = sum( resid2 / (vj+tau_start^2) )
     if ( qmax < threshold ) {
+        # Even largest Q(tau_start) is not enough to reject null.
         return( list( tau=NA, Q=qmax ) )
     }
 
-    stopifnot( tau_start < tau )
 
     if ( ! bracketed ) {
         denom <- vj + tau ^ 2
@@ -67,6 +78,39 @@ find_CI_endpoint = function( resid2, vj, threshold, tau_start, tau,
 
 
 
+#' Calculate cross site variation
+#'
+#' Given a set of impact estimates and associated standard errors, use method of
+#' moments to calculate the implied cross site variation, assuming the standard
+#' errors are correct (and that site-specific impacts are independent of
+#' precision).
+#'
+#' For discussion of this approach, see pg 241 of Handbook of Quantitative
+#' Methods for Educational Research, Timothy Teo (Ed.)
+#'
+#' @param Y List of estimated impacts for sites
+#' @param SE List of standard errors for the estimated impacts.
+#'
+#' @return Grand mean, Q statistic, and estimated tau (cross site variation).
+#' @export
+calc_cross_site_variation = function( Y, SE ) {
+    J = length( Y )
+    stopifnot( length(SE) == J )
+
+    w = 1 / SE^2
+    Ybar = weighted.mean( Y, w )
+    Q = sum( (Y-Ybar)^2 * w )
+    a = sum( w ) - sum( w^2 ) / sum( w )
+
+    if ( Q < J - 1 ) {
+        return( tibble( Ybar = Ybar, Q = Q, tau = 0 ) )
+    } else {
+        return( tibble( Ybar = Ybar, Q = Q, tau = (Q - J + 1) / a ) )
+    }
+}
+
+
+
 
 
 #' Calculate confidence interval for cross site variation using Q-statistic test
@@ -78,16 +122,23 @@ find_CI_endpoint = function( resid2, vj, threshold, tau_start, tau,
 #'
 #' The first simply calculates ATE_hat and SE_hat, and then calls the second.
 #'
-#' Code based on prior work of Catherine Armstrong.  Augmented to do binary
-#' search for endpoints for the test inversion procedure to generate confidence
-#' intervals. Also calls optim() to find the point estimate by maximizing
-#' p-value.
+#' Cross site variation, by default, is estimated by method of moments technique
+#' of Konstantopoulos and Hedges). One can set an alternate method for
+#' estimating cross-site variation via using optim() to find the point estimate
+#' corresponding to maximizing the p-value (this differs slightly from direct
+#' method of moment); this latter approach requires calculating the confidence
+#' interval using test inversion as well.
+#'
+#' Code based on prior work of Catherine Armstrong, which was then augmented to
+#' do binary search for endpoints for the test inversion procedure to generate
+#' confidence intervals.
 #'
 #' @param ATE_hat List of estimated ATEs, one for each site.
 #' @param SE_hat List of associated estimated SEs, assumed to be known.
 #' @param alpha The level of the test.  The CI will be a 1-2alpha confidence
 #'   interval.
-#' @param calc_CI Logical
+#' @param calc_CI Logical, TRUE means calculate the confidence interval using
+#'   test-inversion.
 #' @param verbose Print out a few diagnostics as the CI is being calculated, for
 #'   those curious.
 #' @param tolerance For optimization and search for CI endpoints, how close to
@@ -95,9 +146,15 @@ find_CI_endpoint = function( resid2, vj, threshold, tau_start, tau,
 #' @param mean_method How to calculate the mean effect.  'weighted' is a
 #'   precision weighted mean. 'raw' is the simple mean.  Or pass a number which
 #'   will be used as the mean.
-#' @return List of several estimated quantities: cross-site variation, p-value
-#'   for any variation, whether the p-value is less than passed alpha, the Q
-#'   statistic itself, and the confidence interval range.
+#' @param calc_optim_pvalue TRUE if estimate cross site variation by maximizing
+#'   p-value (a type of Hodges-Lehman estimate).  FALSE means uses
+#'   Konstantopoulos and Hedges direct formula. (TRUE requires calc_CI also be
+#'   set to TRUE.)  Default of FALSE.  FALSE recommended.
+#'
+#' @return List of several estimated quantities: cross-site variation (standard
+#'   deviation, not variance), p-value for presence of any variation, flag of
+#'   whether the p-value is less than passed alpha, the Q statistic itself, and
+#'   the confidence interval.
 #'
 #' @export
 #' @rdname analysis_Qstatistic
@@ -105,9 +162,12 @@ find_CI_endpoint = function( resid2, vj, threshold, tau_start, tau,
 #' analysis_Qstatistic_stat( rnorm(30, sd=0.1), rep(0.1,30), calc_CI = TRUE )
 analysis_Qstatistic_stat <- function( ATE_hat, SE_hat, alpha = 0.05,
                                       calc_CI = FALSE,
+                                      calc_optim_pvalue = FALSE,
                                       verbose = FALSE,
                                       tolerance = 0.00001,
                                       mean_method = c( "weighted", "raw" ) ) {
+
+    stopifnot( length( ATE_hat ) == length( SE_hat ) )
 
     bj = ATE_hat
     vj = SE_hat^2
@@ -115,9 +175,8 @@ analysis_Qstatistic_stat <- function( ATE_hat, SE_hat, alpha = 0.05,
     s <- length(ATE_hat)
 
     if ( !is.numeric( mean_method ) ) {
-        stopifnot( mean_method %in% c( "weighted", "raw" ) )
-
         mean_method = match.arg(mean_method)
+
         if ( mean_method == "weighted" ) {
             bbar <- sum(wj * bj) / (sum(wj))
         } else {
@@ -127,7 +186,12 @@ analysis_Qstatistic_stat <- function( ATE_hat, SE_hat, alpha = 0.05,
         bbar = mean_method
     }
 
-
+    if ( calc_optim_pvalue ) {
+        if (!calc_CI) {
+            warning( "Cannot calc optim p-value without calculating confidence interval.  Setting calc_CI to true.")
+            calc_CI = TRUE
+        }
+    }
 
 
     q <- sum((bj - bbar) ^ 2 / vj)
@@ -141,8 +205,19 @@ analysis_Qstatistic_stat <- function( ATE_hat, SE_hat, alpha = 0.05,
         print( summary( wj ) )
     }
 
-    stopifnot( length( ATE_hat ) == length( SE_hat ) )
 
+    # Estimate cross site variation
+    tau_hat = NA
+    if ( !calc_optim_pvalue ) {
+        a = sum( wj ) - sum( wj^2 ) / sum( wj )
+        if ( q > s - 1 ) {
+            tau_hat = sqrt( (q - s + 1) / a )
+        } else {
+            tau_hat = 0
+        }
+    }
+
+    # Calculate confidence interval.
     if (calc_CI) {
         lowQ <- qchisq(alpha, s - 1)
         highQ <- qchisq(1 - alpha, df = (s - 1))
@@ -154,6 +229,7 @@ analysis_Qstatistic_stat <- function( ATE_hat, SE_hat, alpha = 0.05,
                                       tau_start=0, tau=1,
                                       tolerance=tolerance)
         ts = ifelse( is.na( low_bound$tau ), 0, low_bound$tau )
+
         high_bound = find_CI_endpoint( resid2, vj, threshold=lowQ,
                                        tau_start=ts, tau=pmax( 1, ts * 2 ),
                                        tolerance=tolerance)
@@ -163,7 +239,6 @@ analysis_Qstatistic_stat <- function( ATE_hat, SE_hat, alpha = 0.05,
             CI_low = 0
             CI_high = 0
             tau_hat = 0
-
         } else {
             if ( verbose ) {
                 scat( "Found CI: %.2f (%.2f) - %.2f (%.2f)\n",
@@ -173,26 +248,34 @@ analysis_Qstatistic_stat <- function( ATE_hat, SE_hat, alpha = 0.05,
             CI_low = ts
             CI_high = high_bound$tau
 
-            tau_hat = NA
-            if ( !is.na( low_bound$tau ) ) {
-                tau_start = (low_bound$tau + high_bound$tau)/2
-                rs = optim( tau_start, find_p_value, resid2=resid2, vj=vj,
-                            lower = low_bound$tau, upper = high_bound$tau,
-                            method="L-BFGS-B",
-                            control = list( fnscale = -1 ) )
-                tau_hat = rs$par
-            } else {
-                tau_hat = 0
+            if ( calc_optim_pvalue ) {
+                tau_hat = NA
+                if ( !is.na( low_bound$tau ) ) {
+                    tau_start = (low_bound$tau + high_bound$tau)/2
+                    rs = optim( tau_start, find_p_value, resid2=resid2, vj=vj,
+                                lower = low_bound$tau, upper = high_bound$tau,
+                                method="L-BFGS-B",
+                                control = list( fnscale = -1 ) )
+                    tau_hat = rs$par
+                } else {
+                    tau_hat = 0
+                }
             }
         }
     } else { # we didn't bother calculating the Conf Int.
-        tau_hat = NA
         CI_low = NULL
         CI_high = NULL
     }
 
-    return(list( ATE_hat = bbar, tau_hat = tau_hat, p.value = pval, reject = reject,
-                Q = q, CI_low = CI_low, CI_high = CI_high))
+    res <- list( ATE_hat = bbar, tau_hat = tau_hat,
+                 p.value = pval, reject = reject,
+                 Q = q, CI_low = CI_low, CI_high = CI_high)
+    attr( res, "args" ) = list( alpha=alpha,
+                                calc_optim_pvalue = calc_optim_pvalue,
+                                tolerance = tolerance,
+                                mean_method = mean_method )
+    class( res ) = "qstatresult"
+    return( res )
 }
 
 
@@ -259,3 +342,69 @@ analysis_Qstatistic <- function(Yobs, Z, B, siteID = NULL, data = NULL,
 
     analysis_Qstatistic_stat( ATE_hat, SE_hat, alpha=alpha, calc_CI = calc_CI)
 }
+
+
+
+
+scat = function( str, ... ) {
+    cat( sprintf( str, ... ) )
+}
+
+
+#' Pretty print result from Q-statistic call
+#'
+#' @export
+#' @param x A qstatresult object.
+#' @param ... No extra options passed.
+#' @family qstatresult
+print.qstatresult = function( x, ... ) {
+    args = attr( x, "args" )
+
+    cat( "Cross-site Q-statistic analysis\n" )
+    scat( "  Grand Average: %s\n", format( x$ATE_hat ) )
+    scat( "  Cross-site SD: %s",
+          format( x$tau_hat ) )
+    if ( is.null( x$CI_low ) && is.null( x$CI_high ) ) {
+        cat("\n" )
+    } else {
+        scat( " (%s%% CI %s--%s)\n",
+          format( 100*(1-args$alpha*2) ),
+          format( x$CI_low ), format( x$CI_high ) )
+    }
+    scat( "  Q = %s, p=%.4f\n", format( x$Q ), x$p.value )
+
+    est_method = ifelse( args$calc_optim_pvalue, "max p-value", "MoM" )
+    scat( "  [ tolerance = %s; mean method = %s; est method = %s ]\n",
+         format( args$tolerance, digits=2 ), args$mean_method,
+         est_method )
+
+    invisible( x )
+}
+
+
+
+#' Is object a qstatresult object?
+#'
+#' @export
+#' @aliases qstatresult
+#' @param x the object to check.
+#' @family qstatresult
+is.qstatresult = function( x ) {
+    inherits(x, "qstatresult")
+}
+
+
+
+
+#' Cast qstat result to data.frame
+#'
+#' @export
+#' @aliases qstatresult
+#' @param x the qstatresult object to covert
+#' @family qstatresult
+as.data.frame.qstatresult = function( x ) {
+    class(x) = "list"
+    as.data.frame( x )
+}
+
+
--- tests/testthat/test-analysis_Qstatistic.R
+++ tests/testthat/test-analysis_Qstatistic.R
@@ -1,26 +1,33 @@
+
+
+
 test_that("Q stat test inversion works", {
 
-
-    # More Demo of code working
-
     # Small amounts of variation, but very precise
+    set.seed( 1010101 )
     SE_hat = rep(0.005,30)
-    ATE_hat = rnorm( 30, sd = 0.2 ) + rnorm( 30, sd=SE_hat )
+    ATE_hat = rnorm( 30, sd = 0.2 ) + rnorm( 30, sd = SE_hat )
     rs = analysis_Qstatistic_stat( ATE_hat, SE_hat, calc_CI = TRUE )
     rs
     expect_true( rs$tau_hat > 0 )
     expect_true( rs$p.value < 0.01 )
+    expect_true( rs$CI_low <= 0.2 )
+    expect_true( rs$CI_high >= 0.2 )
 
     # Even smaller amounts of variation, but very precise
-    SE_hat = rep(0.01,300)
+    SE_hat = rep(0.01, 300)
     ATE_hat = rnorm( 300, sd = 0.01 ) + rnorm( 300, sd=SE_hat )
     rs = analysis_Qstatistic_stat( ATE_hat, SE_hat, calc_CI = TRUE )
     rs
     expect_true( rs$tau_hat > 0 )
+    expect_true( abs( rs$tau_hat - 0.01 ) < 0.01 )
     expect_true( rs$p.value < 0.01 )
+    expect_true( rs$CI_high >= 0.01 )
+    expect_true( rs$CI_low <= 0.01 )
+    expect_true( rs$CI_low > 0.01^2 )
 
-
-    # Not much variation but lots of thought to exist variation
+    # Not much variation but lots of thought to exist variation (so we would be
+    # up against a boundary of no variation)
     A = rnorm( 40 )
     rs = analysis_Qstatistic_stat( A, rep( 1.1, 40 ), calc_CI = TRUE )
     rs
@@ -28,7 +35,8 @@ test_that("Q stat test inversion works", {
     expect_true( rs$CI_low == 0 )
     expect_true( rs$p.value > 0.05 )
 
-    # Not much variation but lots and lots of thought to exist variation
+    # Not much variation but lots and lots of thought to exist variation (so we
+    # are sure there is no possible cross site variation).
     A = rnorm( 100 )
     rs = analysis_Qstatistic_stat( A, rep( 10, 100 ), calc_CI = TRUE )
     rs
@@ -40,8 +48,37 @@ test_that("Q stat test inversion works", {
 
 
 
-test_that("Q stat weighting methods work", {
 
+test_that("Q stat with massive cross site variation works", {
+
+    set.seed( 1010101 )
+    SE_hat = rnorm( 100, 10, 2 ) / 5
+    ATE_hat = rnorm( 100, sd = 10 ) + rnorm( 100, sd = SE_hat )
+
+    rs = analysis_Qstatistic_stat( ATE_hat, SE_hat, calc_CI = TRUE ) #, verbose = TRUE )
+    rs
+
+    expect_true( abs(rs$tau_hat - 10) <= 0.5 )
+    expect_true( rs$CI_high >= 10 )
+    expect_true( rs$CI_low <= 10 )
+
+
+    rs2 = analysis_Qstatistic_stat( ATE_hat, SE_hat, calc_CI = TRUE,
+                                   calc_optim_pvalue = TRUE ) #, verbose = TRUE )
+    rs2
+
+    expect_true( abs(rs2$tau_hat - 10) <= 0.5 )
+    expect_true( rs2$CI_high >= 10 )
+    expect_true( rs2$CI_low <= 10 )
+
+    expect_true( abs( rs$tau_hat - rs2$tau_hat ) <= 0.25 )
+
+})
+
+
+
+
+test_that("Q stat weighting methods work", {
 
 
     # Variation correlated with precision
@@ -52,22 +89,21 @@ test_that("Q stat weighting methods work", {
 
     rs = analysis_Qstatistic_stat( ATE_hat, SE_hat, calc_CI = TRUE ) #, verbose = TRUE )
     rs
-    rs$ATE_hat = weighted.mean( ATE_hat, w=1/SE_hat^2 )
+    expect_true( rs$ATE_hat == weighted.mean( ATE_hat, w=1/SE_hat^2 ) )
 
     rs2 = analysis_Qstatistic_stat( ATE_hat, SE_hat, calc_CI = TRUE, mean_method = "raw" ) #, verbose = TRUE )
     rs2
-    rs2$ATE_hat = mean(ATE_hat)
+    expect_true( rs2$ATE_hat == mean(ATE_hat) )
 
 
     rs3 = analysis_Qstatistic_stat( ATE_hat, SE_hat, calc_CI = TRUE, mean_method = 1 ) #, verbose = TRUE )
     rs3
-    rs3$ATE_hat = 1
+    expect_true( rs3$ATE_hat == 1 )
 
     alrs = bind_rows( as.data.frame( rs ), as.data.frame( rs2 ), as.data.frame( rs3 ) )
     alrs
 
     expect_true( nrow( alrs ) == 3 )
-
 
 
 })
